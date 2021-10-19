cbe0fa280f8dcf6276819a120dfeb49cf0a6ec37
Stephanie.Gao <tm1997@163.com>
2021-10-19 17:17:50 +0800

    feat: add templates for interfaces

---
+++ sources/templates/interfaces.js
@@ -0,0 +1,21 @@
+module.exports = (options) => {
+  return [{
+    fileName: 'interface/custom.d.ts',
+    content:
+`declare module '*.svg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.module.less' {
+  const content: any;
+  export = content;
+}
+
+declare module '*.less' {
+  const content: any;
+  export = content;
+}
+`,
+  }];
+};
